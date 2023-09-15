8b4f24a758f4b691cd64d6f12f1ba94981236044
woonkit <woonkit.liu.sg@gmail.com>
2023-09-16 00:14:02 +0800

    Added update commands to allow sending of updates position to Android

---
+++ algo/entities/commands/update_command.py
@@ -0,0 +1,38 @@
+import settings
+from entities.assets.direction import Direction
+from entities.commands.command import Command
+from entities.grid.position import Position, RobotPosition
+
+# UPDATE COMMAND ONLY FOR ANDROID
+class UpdateCommand(Command):
+    def __init__(self, position: Position):
+        super().__init__(0)
+        self.position = position
+        print(self.position)
+
+    def __str__(self):
+        return str(self.position)
+
+    __repr__ = __str__
+
+    def process_one_tick(self, robot):
+        if self.total_ticks == 0:
+            return
+
+        self.tick()
+
+    def apply_on_pos(self, curr_pos):
+        pass
+    
+    def convert_to_message(self):
+        
+        match self.position.direction:
+            case Direction.TOP:
+                direction = 'U'
+            case Direction.BOTTOM:
+                direction = 'D'
+            case Direction.LEFT:
+                direction = 'L'
+            case Direction.RIGHT:
+                direction = 'R'
+        return f"U{self.position.x // settings.SCALING_FACTOR}-{self.position.y // settings.SCALING_FACTOR}-{direction}"
