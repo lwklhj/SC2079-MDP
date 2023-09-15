4853599d0c4a96408f2544985b4f5916db83702d
woonkit <woonkit.liu.sg@gmail.com>
2023-09-15 22:23:05 +0800

    - Moved AI model to rpi subfolder - Added picamera2 + Sebas's AI code

--- rpi/finalv2/fastest_car.py
+++ rpi/finalv2/fastest_car.py
@@ -1,3 +1,30 @@
 class FastestCarTask:
     def __init__(self,RPI):
         self.RPI = RPI
+        self.commands = []
+        
+    def start(self):
+        # Activate ultrasonic sensor
+        self.enqueueCommand("uuuuu")
+        self.enqueueCommand("l0020")
+        result = self.RPI.imrec.take_picture()
+        if(result == "left"):
+            self.RPI.stm.send("l0090")
+            self.RPI.stm.send("r0090")
+            self.RPI.stm.send("r0090")
+            self.RPI.stm.send("l0090")
+        else:
+            self.RPI.stm.send("r0090")
+            self.RPI.stm.send("l0090")
+            self.RPI.stm.send("l0090")
+            self.RPI.stm.send("r0090")
+
+        self.RPI.stm.send("l0020")
+
+    def enqueueCommand(self, command):
+        self.commands.push(command)
+
+    def runAndWait(self):
+        while(self.commands.count !=0 ):
+            command = self.commands.pop()
+            self.RPI.stm.send(command)
