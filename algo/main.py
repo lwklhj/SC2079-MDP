1e20f360827b3fa3378f37596038aa76c916b0ee
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2023-09-16 13:05:24 +0800

    Edit the server/client code

--- algo/main.py
+++ algo/main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import pickle
 from typing import List
 
 import settings
@@ -58,38 +59,81 @@ def run_minimal(also_run_simulator):
     print("Connected to RPi!\n")
 
     print("Waiting to receive obstacle data from RPi...")
-    # Create a server to receive information from the RPi.
-    server = RPiServer(settings.PC_HOST, settings.PC_PORT)
-    # Wait for the RPi to connect to the PC.
-    try:
-        server.start()
-    except OSError or KeyboardInterrupt as e:
-        print(e)
-        server.close()
-        client.close()
-        sys.exit(1)
+    # # Create a server to receive information from the RPi.
+    # server = RPiServer(settings.PC_HOST, settings.PC_PORT)
+    # # Wait for the RPi to connect to the PC.
+    # try:
+    #     server.start()
+    # except OSError or KeyboardInterrupt as e:
+    #     print(e)
+    #     server.close()
+    #     client.close()
+    #     sys.exit(1)
 
     # At this point, both the RPi and the PC are connected to each other.
     # Create a synchronous call to wait for RPi data.
-    obstacle_data: list = server.receive_data()
-    server.close()
-    print("Got data from RPi:")
-    print(obstacle_data)
 
-    obstacles = parse_obstacle_data(obstacle_data)
-    if also_run_simulator:
-        app = AlgoSimulator(obstacles)
+    # obstacle_data: list = server.receive_data()
+    # server.close()
+
+    data = []
+    while True:
+        d = client.socket.recv(1024)  # 1024 is the buffer size
+        if not d:
+            break
+        print(d)
+        data.append(d)
+
+        obstacle_data: list = pickle.loads(b''.join(data))
+        print("Got data from RPi:")
+        print(obstacle_data)
+
+        obstacles = parse_obstacle_data(obstacle_data)
+
+        if also_run_simulator:
+            app = AlgoSimulator(obstacles)
+            app.init()
+            app.execute()
+
+        app = AlgoMinimal(obstacles)
         app.init()
         app.execute()
-    app = AlgoMinimal(obstacles)
-    app.init()
-    app.execute()
 
-    # Send the list of commands over.
-    print("Sending list of commands to RPi...")
-    commands = app.robot.convert_all_commands()
-    client.send_message(commands)
+        # Send the list of commands over.
+        print("Sending list of commands to RPi...")
+        commands = app.robot.convert_all_commands()
+        client.send_message(commands)
     client.close()
+
+    # obstacle_data: list = client.receive()
+    # client.close()
+    # print("Got data from RPi:")
+    # print(obstacle_data)
+
+    # # obstacle_data = [
+    # #     [105, 75, 180, 0],
+    # #     [135, 25, 0, 1],
+    # #     [195, 95, 180, 2],
+    # #     [175, 185, -90, 3],
+    # #     [75, 125, 90, 4],
+    # #     [15, 195, -90, 5]
+    # # ]
+    # obstacles = parse_obstacle_data(obstacle_data)
+
+    # if also_run_simulator:
+    #     app = AlgoSimulator(obstacles)
+    #     app.init()
+    #     app.execute()
+
+    # app = AlgoMinimal(obstacles)
+    # app.init()
+    # app.execute()
+
+    # # Send the list of commands over.
+    # print("Sending list of commands to RPi...")
+    # commands = app.robot.convert_all_commands()
+    # client.send_message(commands)
+    # client.close()
 
 
 def run_rpi():
@@ -99,5 +143,5 @@ def run_rpi():
 
 
 if __name__ == '__main__':
-    # run_minimal(True)
-    run_simulator()
+    run_minimal(False)
+    # run_simulator()
