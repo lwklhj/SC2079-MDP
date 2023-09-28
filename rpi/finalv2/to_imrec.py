a4a226a560d23b494557a8bbc0287bf4f2e015ef
woonkit <woonkit.liu.sg@gmail.com>
2023-09-28 15:57:58 +0800

    000

--- rpi/finalv2/to_imrec.py
+++ rpi/finalv2/to_imrec.py
@@ -3,8 +3,10 @@
 import socket
 import threading
 from picamera2 import Picamera2
-import tensorflow as tf
+from ultralytics import YOLO
+from PIL import Image
 import numpy as np
+import os
 
 class imrecInterface:
     def __init__(self, RPI):
@@ -12,11 +14,12 @@ class imrecInterface:
         # self.clientSocket = socket.socket()
         # self.sender = None
         # self.attempts = 0
-        TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
-        self.interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+        TF_MODEL_FILE_PATH = 'yolov8n.pt' # The default path to the saved TensorFlow Lite model
+        self.model = YOLO(TF_MODEL_FILE_PATH)
         self.picam2 = Picamera2()
-        config = self.picam2.create_still_configuration(main={"size": (180, 180), "format": "BGR888"})
+        config = self.picam2.create_still_configuration(main={"size": (640, 640), "format": "BGR888"})
         self.picam2.configure(config)
+        self.i = 0
     
     # Defunct
     def connectImrec(self):
@@ -65,42 +68,25 @@ class imrecInterface:
             
             
 
-    def take_picture(self):
-        # sender = imagezmq.ImageSender(connect_to='tcp://192.168.20.25:5555')
-        # cam = cv2.VideoCapture(0)
-        # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        # rpi_name = socket.gethostname()
-        # check, image = cam.read()
-        # try:
-        #     sender.send_image(rpi_name, image)
-        #     print("Image sent to IMREC server!")
-        # except Exception as e:
-        #     print("Image sending failed!")
-        
+    def take_picture(self):        
         class_names = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40']
 
         self.picam2.start()
-        img = self.picam2.capture_image("main")
+        img = self.picam2.capture_array("main")
+        results = self.model.predict(img, save = True, imgsz=640, conf=0.5, save_txt=True, save_conf=True)
 
-        # Image size
-        # img_height = 180
-        # img_width = 180
+        print(results[0].save_dir)
+        # for file in os.listdir(results[0].save_dir+'/labels'):
+        #     if file.endswith('.txt'):
+        #         print(file)
+        #         with open(r.save_dir+'/labels/'+file, 'r') as f:
+        #             lines = f.readlines()
+        #             if lines:
+        #                 first_integer = int(lines[0].split()[0])
+        #                 print("Detected image:", classes[first_integer])
+        #             else:
+        #                 print("No lines in file:", file)
 
-        #img = tf.keras.utils.load_img(
-        #    image_path, target_size=(img_height, img_width)
-        #)
-        img_array = tf.keras.utils.img_to_array(img)
-        img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-        classify_lite = self.interpreter.get_signature_runner('serving_default')
-
-        predictions_lite = classify_lite(rescaling_2_input=img_array)['dense_1']
-        score_lite = tf.nn.softmax(predictions_lite)
-
-        # print(
-        #     "This image most likely belongs to {} with a {:.2f} percent confidence."
-        #     .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
-
-        print(class_names[np.argmax(score_lite)])
-        return class_names[np.argmax(score_lite)]
+        # print(class_names[np.argmax(score_lite)])
+        # return class_names[np.argmax(score_lite)]
+        return 0
