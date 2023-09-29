911ea6fd19272eb8055fd24a6ad204cb17ad9492
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2023-09-29 14:37:35 +0800

    Editting the bullseye for 3x3 turn

--- rpi/finalv2/bullseye.py
+++ rpi/finalv2/bullseye.py
@@ -9,11 +9,15 @@ class bullseyeInterface:
         count = 1
 
         while(result == "Back" and count!=4):
-                self.RPI.stm.send("b0030")
+                self.RPI.stm.send("b0060")
                 self.RPI.stm.send("r0090")
-                self.RPI.stm.send("f0010")
                 self.RPI.stm.send("l0090")
                 self.RPI.stm.send("l0090")
                 result = self.RPI.imrec.take_picture()
                 count = count + 1
 
+# 'b0010', 'U65.0-105.0-E', 'b0010', 'U55.0-105.0-E', 
+# 'b0010', 'U45.0-105.0-E', 'b0010', 'U35.0-105.0-E', 
+# 'b0010', 'U25.0-105.0-E', 'b0010', 'U15.0-105.0-E', 
+# 'r0090', 'U45.0-75.0-S', 'l0090', 'U75.0-45.0-E', 
+# 'l0090', 'U105.0-75.0-N', 's0000'
