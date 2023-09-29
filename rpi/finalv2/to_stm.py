add8d2aaab103faf174144ba22793e1a08a22df8
woonkit <woonkit.liu.sg@gmail.com>
2023-09-29 10:15:59 +0800

    000000

--- rpi/finalv2/to_stm.py
+++ rpi/finalv2/to_stm.py
@@ -5,7 +5,7 @@ class STMInterface:
     def __init__(self,RPI):
         self.RPI = RPI
         try:
-            self.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
+            self.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
             print("Connected to STM via USB 0")
         except:
             try:
