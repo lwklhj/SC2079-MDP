74a518daacfe7b3fae41e407de8b40939f3abf40
woonkit <woonkit.liu.sg@gmail.com>
2023-10-02 13:33:21 +0800

    Attempt to fix reception of image file in algo server

--- algo/main.py
+++ algo/main.py
@@ -75,66 +75,134 @@ def run_minimal(also_run_simulator):
     # obstacle_data: list = server.receive_data()
     # server.close()
 
-    # data = []
-
+    buffer = []
     while True:
-        data = client.socket.recv(40960000)  # 40960000 is the buffer size
-        data = data.decode()  # convert to string
+        received = client.socket.recv(4096)  # 4096 is the buffer size
 
-        print("Got data from RPi:")
-        # print(data)
-        # Check if image
-        if (data.split('|')[0] == "img"):
-            try:
-                index = data.split('|')[0][4:-1]
-                img = pickle.loads(base64.b64decode(data.split(':')[1]))
-                # Load Model
-                MODEL_FILE_PATH = 'best.pt'
-                model = YOLO(MODEL_FILE_PATH)
-                # Start imrec
-                results = model.predict(img, save=True, imgsz=640, conf=0.5, save_txt=True,
-                                        save_conf=True, project="/Users/jordan/Documents/Github/SC2079-MDP/algo")
-                classes = results[0].names
-                for file in os.listdir(results[0].save_dir+'/labels'):
-                    if file.endswith('.txt'):
-                        with open(results[0].save_dir+'/labels/'+file, 'r') as f:
-                            lines = f.readlines()
-                            if lines:
-                                first_integer = int(lines[0].split()[0])
-                                print("Detected image:",
-                                      classes[first_integer])
-                # Send result back
-                client.send_message(f"imgID|{index}|{classes[first_integer]}")
-            except Exception as e:
-                print("IMAGE RECOGNITION ERROR: ", e)
+        # No more incoming bytes
+        # Start operations
+        if not received:
+            # Join bytes into byte string
+            data = b''.join(buffer)
+            data = data.decode()
 
-        else:
-            data = data.split(',')  # Split on delimiter
+            # Reset buffer
+            buffer.clear()
 
-            obstacle_data = []  # Array for storing obstacle data
+            if (data.split('|')[0] == "img"):
+                try:
+                    index = data.split('|')[1]
+                    img = pickle.loads(base64.b64decode(data.split('|')[2]))
+                    # Load Model
+                    MODEL_FILE_PATH = 'best.pt'
+                    model = YOLO(MODEL_FILE_PATH)
+                    # Start imrec
+                    results = model.predict(img, save=True, imgsz=640, conf=0.5, save_txt=True,
+                                            save_conf=True, project="/Users/jordan/Documents/Github/SC2079-MDP/algo")
+                    classes = results[0].names
+                    for file in os.listdir(results[0].save_dir+'/labels'):
+                        if file.endswith('.txt'):
+                            with open(results[0].save_dir+'/labels/'+file, 'r') as f:
+                                lines = f.readlines()
+                                if lines:
+                                    first_integer = int(lines[0].split()[0])
+                                    print("Detected image:",
+                                        classes[first_integer])
+                    # Send result back
+                    client.send_message(f"imgID|{index}|{classes[first_integer]}")
+                except Exception as e:
+                    print("IMAGE RECOGNITION ERROR: ", e)
 
-            i = 0
-            while (i < len(data)):
-                # Android formatted the data as below
-                # Ori,x,y,no,ori2,x2,y2,no2
-                obstacle_data.append(
-                    (int(data[i]), int(data[i+1]), int(data[i+2]), int(data[i+3])))
-                i += 4  # Every four strings is an obstacle
-            obstacles = parse_obstacle_data(obstacle_data)
+            else:
+                data = data.split(',')  # Split on delimiter
 
-            if also_run_simulator:
-                app = AlgoSimulator(obstacles)
+                obstacle_data = []  # Array for storing obstacle data
+
+                i = 0
+                while (i < len(data)):
+                    # Android formatted the data as below
+                    # Ori,x,y,no,ori2,x2,y2,no2
+                    obstacle_data.append(
+                        (int(data[i]), int(data[i+1]), int(data[i+2]), int(data[i+3])))
+                    i += 4  # Every four strings is an obstacle
+                obstacles = parse_obstacle_data(obstacle_data)
+
+                if also_run_simulator:
+                    app = AlgoSimulator(obstacles)
+                    app.init()
+                    threading.Thread(target=app.execute).start()
+
+                app = AlgoMinimal(obstacles)
                 app.init()
-                threading.Thread(target=app.execute).start()
+                app.execute()
 
-            app = AlgoMinimal(obstacles)
-            app.init()
-            app.execute()
+                # Send the list of commands over.
+                print("Sending list of commands to RPi...")
+                commands = app.robot.convert_all_commands()
+                client.send_message(commands)
+        
+        # If not empty, append to buffer
+        else:
+            buffer.append(received)
 
-            # Send the list of commands over.
-            print("Sending list of commands to RPi...")
-            commands = app.robot.convert_all_commands()
-            client.send_message(commands)
+
+        
+        # data = data.decode()  # convert to string
+
+        # print("Got data from RPi:")
+        # # print(data)
+        # # Check if image
+        # if (data.split('|')[0] == "img"):
+        #     try:
+        #         index = data.split('|')[0][4:-1]
+        #         img = pickle.loads(base64.b64decode(data.split(':')[1]))
+        #         # Load Model
+        #         MODEL_FILE_PATH = 'best.pt'
+        #         model = YOLO(MODEL_FILE_PATH)
+        #         # Start imrec
+        #         results = model.predict(img, save=True, imgsz=640, conf=0.5, save_txt=True,
+        #                                 save_conf=True, project="/Users/jordan/Documents/Github/SC2079-MDP/algo")
+        #         classes = results[0].names
+        #         for file in os.listdir(results[0].save_dir+'/labels'):
+        #             if file.endswith('.txt'):
+        #                 with open(results[0].save_dir+'/labels/'+file, 'r') as f:
+        #                     lines = f.readlines()
+        #                     if lines:
+        #                         first_integer = int(lines[0].split()[0])
+        #                         print("Detected image:",
+        #                               classes[first_integer])
+        #         # Send result back
+        #         client.send_message(f"imgID|{index}|{classes[first_integer]}")
+        #     except Exception as e:
+        #         print("IMAGE RECOGNITION ERROR: ", e)
+
+        # else:
+        #     data = data.split(',')  # Split on delimiter
+
+        #     obstacle_data = []  # Array for storing obstacle data
+
+        #     i = 0
+        #     while (i < len(data)):
+        #         # Android formatted the data as below
+        #         # Ori,x,y,no,ori2,x2,y2,no2
+        #         obstacle_data.append(
+        #             (int(data[i]), int(data[i+1]), int(data[i+2]), int(data[i+3])))
+        #         i += 4  # Every four strings is an obstacle
+        #     obstacles = parse_obstacle_data(obstacle_data)
+
+        #     if also_run_simulator:
+        #         app = AlgoSimulator(obstacles)
+        #         app.init()
+        #         threading.Thread(target=app.execute).start()
+
+        #     app = AlgoMinimal(obstacles)
+        #     app.init()
+        #     app.execute()
+
+        #     # Send the list of commands over.
+        #     print("Sending list of commands to RPi...")
+        #     commands = app.robot.convert_all_commands()
+        #     client.send_message(commands)
     client.close()
 
     # obstacle_data: list = client.receive()
