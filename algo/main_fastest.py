16bb01e17d80647ee79fb5b8624ecd7a8ae02b12
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2023-10-17 17:30:58 +0800

    Hard code for testing to set first_integer

--- algo/main_fastest.py
+++ algo/main_fastest.py
@@ -3,7 +3,7 @@ import time
 import pickle
 from typing import List
 
-import image_join
+import image_join as image_join
 import settings
 from app import AlgoSimulator, AlgoMinimal
 from entities.assets.direction import Direction
@@ -104,94 +104,6 @@ def run_minimal(also_run_simulator):
                     img = pickle.loads(base64.b64decode(image_data_base64))
                     print("3rd")
                     # Load Model
-                    MODEL_FILE_PATH = '/Users/tirthoza/Desktop/MDP/SC2079-MDP/algo/best.pt'
-                    model = YOLO(MODEL_FILE_PATH)
-                    folder_path = "/Users/tirthoza/Desktop/MDP/SC2079-MDP/algo/predictions"
-                    # Start imrec
-                    results = model.predict(img, save=True, imgsz=640, conf=0.5, save_txt=True,
-                                            save_conf=True, project=folder_path)
-                    classes = results[0].names
-                    for file in os.listdir(results[0].save_dir+'/labels'):
-                        if file.endswith('.txt'):
-                            with open(results[0].save_dir+'/labels/'+file, 'r') as f:
-                                lines = f.readlines()
-                                if lines:
-                                    first_integer = int(lines[0].split()[0])
-                                    print(first_integer)
-                                    print("Detected image:",
-                                          classes[first_integer])
-                    
-                    #Navigating the first obstacle
-                    if count_scans == 0:
-                        if results[0].names == "Right":
-                            movement_list = ["r0090", "l0090", "b0020", "l0090", "r0090"]
-                            client.send_message(movement_list)
-
-                        elif results[0].names == "Left":
-                            movement_list = ["l0090", "r0090", "b0020", "r0090", "l0090"]
-                            client.send_message(movement_list)
-                    
-                    #Navigating second obstacle
-                    if count_scans == 1:
-                        if results[0].names == "Right":
-                            movement_list = ["r0090", "f0020", "l0090", "l0090"]
-                            client.send_message(movement_list)
-
-                        elif results[0].names == "Left":
-                            movement_list = ["l0090"]
-                            client.send_message(movement_list)
-                    
-                    
-                    count_scans += 1
-                    if(2==count_scans):
-                        image_join.collate_images(folder_path)
-                except Exception as e:
-                    print("IMAGE RECOGNITION ERROR: ", e)
-
-                
-
-        #send command and add it in a stack as well, to use the stack for the return process.
-
-
-    while True:
-        received = client.socket.recv(64768000)  # 4096 is the buffer size
-        buffer.append(received)
-        # Join bytes into byte string
-        data = b''.join(buffer)
-        data = data.decode()
-        # No more incoming bytes
-        # Start operations
-        if "!" in data:
-            # Reset buffer
-            buffer.clear()
-            data = data.replace("!", "")
-            print(type(data))
-            # print(data)
-            if (data.split('|')[0] == "img"):
-                try:
-                    
-                    print("1st")
-                    index = data.split('|')[1]
-                    print("2nd")
-                    image_data_base64 = data.split('|')[2]
-
-                    print(len(image_data_base64))
-
-                    image_data_base64 = image_data_base64[2:len(
-                        image_data_base64)-1]
-
-                    # buffer needed for multiple of 4
-                    # buffer_needed = 4 -((len(image_data_base64)) %4)
-                    # image_data_base64 += '=' * buffer_needed
-
-                    print(image_data_base64[:50])
-                    print(image_data_base64[-50:])
-                    print(len(image_data_base64))
-                    print(type(image_data_base64))
-
-                    img = pickle.loads(base64.b64decode(image_data_base64))
-                    print("3rd")
-                    # Load Model
                     MODEL_FILE_PATH = '/Users/jordan/Documents/Github/SC2079-MDP/algo/best.pt'
                     model = YOLO(MODEL_FILE_PATH)
                     folder_path = "/Users/jordan/Documents/Github/SC2079-MDP/algo/predictions"
@@ -208,45 +120,136 @@ def run_minimal(also_run_simulator):
                                     print(first_integer)
                                     print("Detected image:",
                                           classes[first_integer])
-                    # Send result back
-                    response = f"imgID|{index}|{classes[first_integer]}"
-                    # if (len(data.split("|") == 4)):
-                    #     response += f"|{data.split('|')[3]}"
-
-                    client.send_message(response)
+                    
+                    first_integer = 38
+                    #Navigating the first obstacle
+                    if count_scans == 0:
+                        # Go right
+                        if classes[first_integer] == 38:
+                            movement_list = ["r0090", "l0090", "b0020", "l0090", "r0090"]
+                            client.send_message(movement_list)
+                        # Go left
+                        elif classes[first_integer] == 39:
+                            movement_list = ["l0090", "r0090", "b0020", "r0090", "l0090"]
+                            client.send_message(movement_list)
+                    
+                    #Navigating second obstacle
+                    if count_scans == 1:
+                        # Go right
+                        if classes[first_integer] == 38:
+                            movement_list = ["r0090", "f0020", "l0090", "l0090"]
+                            client.send_message(movement_list)
+                        # Go left
+                        elif classes[first_integer] == 39:
+                            movement_list = ["l0090"]
+                            client.send_message(movement_list)
+                    
+                    
                     count_scans += 1
-                    if(len(obstacles)==count_scans):
+                    if(2==count_scans):
                         image_join.collate_images(folder_path)
                 except Exception as e:
                     print("IMAGE RECOGNITION ERROR: ", e)
 
-            else:
-                data = data.split(',')  # Split on delimiter
+                
 
-                obstacle_data = []  # Array for storing obstacle data
+        #send command and add it in a stack as well, to use the stack for the return process.
 
-                i = 0
-                while (i < len(data)):
-                    # Android formatted the data as below
-                    # Ori,x,y,no,ori2,x2,y2,no2
-                    obstacle_data.append(
-                        (int(data[i]), int(data[i+1]), int(data[i+2]), int(data[i+3])))
-                    i += 4  # Every four strings is an obstacle
-                obstacles = parse_obstacle_data(obstacle_data)
 
-                if also_run_simulator:
-                    app = AlgoSimulator(obstacles)
-                    app.init()
-                    threading.Thread(target=app.execute).start()
+    # while True:
+    #     received = client.socket.recv(64768000)  # 4096 is the buffer size
+    #     buffer.append(received)
+    #     # Join bytes into byte string
+    #     data = b''.join(buffer)
+    #     data = data.decode()
+    #     # No more incoming bytes
+    #     # Start operations
+    #     if "!" in data:
+    #         # Reset buffer
+    #         buffer.clear()
+    #         data = data.replace("!", "")
+    #         print(type(data))
+    #         # print(data)
+    #         if (data.split('|')[0] == "img"):
+    #             try:
+                    
+    #                 print("1st")
+    #                 index = data.split('|')[1]
+    #                 print("2nd")
+    #                 image_data_base64 = data.split('|')[2]
 
-                app = AlgoMinimal(obstacles)
-                app.init()
-                app.execute()
+    #                 print(len(image_data_base64))
 
-                # Send the list of commands over.
-                print("Sending list of commands to RPi...")
-                commands = app.robot.convert_all_commands()
-                client.send_message(commands)
+    #                 image_data_base64 = image_data_base64[2:len(
+    #                     image_data_base64)-1]
+
+    #                 # buffer needed for multiple of 4
+    #                 # buffer_needed = 4 -((len(image_data_base64)) %4)
+    #                 # image_data_base64 += '=' * buffer_needed
+
+    #                 print(image_data_base64[:50])
+    #                 print(image_data_base64[-50:])
+    #                 print(len(image_data_base64))
+    #                 print(type(image_data_base64))
+
+    #                 img = pickle.loads(base64.b64decode(image_data_base64))
+    #                 print("3rd")
+    #                 # Load Model
+    #                 MODEL_FILE_PATH = '/Users/jordan/Documents/Github/SC2079-MDP/algo/best.pt'
+    #                 model = YOLO(MODEL_FILE_PATH)
+    #                 folder_path = "/Users/jordan/Documents/Github/SC2079-MDP/algo/predictions"
+    #                 # Start imrec
+    #                 results = model.predict(img, save=True, imgsz=640, conf=0.5, save_txt=True,
+    #                                         save_conf=True, project=folder_path)
+    #                 classes = results[0].names
+    #                 for file in os.listdir(results[0].save_dir+'/labels'):
+    #                     if file.endswith('.txt'):
+    #                         with open(results[0].save_dir+'/labels/'+file, 'r') as f:
+    #                             lines = f.readlines()
+    #                             if lines:
+    #                                 first_integer = int(lines[0].split()[0])
+    #                                 print(first_integer)
+    #                                 print("Detected image:",
+    #                                       classes[first_integer])
+    #                 # Send result back
+    #                 response = f"imgID|{index}|{classes[first_integer]}"
+    #                 # if (len(data.split("|") == 4)):
+    #                 #     response += f"|{data.split('|')[3]}"
+
+    #                 client.send_message(response)
+    #                 count_scans += 1
+    #                 if(len(obstacles)==count_scans):
+    #                     image_join.collate_images(folder_path)
+    #             except Exception as e:
+    #                 print("IMAGE RECOGNITION ERROR: ", e)
+
+    #         else:
+    #             data = data.split(',')  # Split on delimiter
+
+    #             obstacle_data = []  # Array for storing obstacle data
+
+    #             i = 0
+    #             while (i < len(data)):
+    #                 # Android formatted the data as below
+    #                 # Ori,x,y,no,ori2,x2,y2,no2
+    #                 obstacle_data.append(
+    #                     (int(data[i]), int(data[i+1]), int(data[i+2]), int(data[i+3])))
+    #                 i += 4  # Every four strings is an obstacle
+    #             obstacles = parse_obstacle_data(obstacle_data)
+
+    #             if also_run_simulator:
+    #                 app = AlgoSimulator(obstacles)
+    #                 app.init()
+    #                 threading.Thread(target=app.execute).start()
+
+    #             app = AlgoMinimal(obstacles)
+    #             app.init()
+    #             app.execute()
+
+    #             # Send the list of commands over.
+    #             print("Sending list of commands to RPi...")
+    #             commands = app.robot.convert_all_commands()
+    #             client.send_message(commands)
 
 
 def run_rpi():
@@ -256,5 +259,5 @@ def run_rpi():
 
 
 if __name__ == '__main__':
-    #run_minimal(True)
-    run_simulator()
+    run_minimal(False)
+    # run_simulator()
